ad77a66600b4be6c65014f598f8704bc33302f56
7doublem <mustafamoallim@gmail.com>
2025-07-31 23:22:29 +0100

    Binary Search List

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,15 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let lo = 0;
+    let hi = haystack.length;
+    while (lo < hi) {
+        const m = Math.floor((lo + hi)/2)
+        if (haystack[m] === needle) {
+            return true;
+        } else if (haystack[m] < needle) {
+            lo = m + 1
+        } else {
+            hi = m;
+        }
+    }
+    return false;
 }
