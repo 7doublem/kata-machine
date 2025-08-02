7eb60917bffdd9de218a82caf73c5b920faebb8d
7doublem <mustafamoallim@gmail.com>
2025-08-03 00:52:04 +0100

    bubble sort and queue

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,6 +1,5 @@
 export default function bubble_sort(arr: number[]): void {
 
-    console.log(arr)
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length - i - 1; j++) {
             if (arr[j] > arr[j+1]) {
