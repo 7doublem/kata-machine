6077aea07be168ed9a09a003e3e82c03dcf34534
7doublem <mustafamoallim@gmail.com>
2025-08-02 22:24:25 +0100

    bubble sort and two crystal ball

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,22 @@
-export default function two_crystal_balls(breaks: boolean[]): number {
+// given two crystal balls that will break if dropped from high enough distance
+// determine the exact spot that it will break in the most optimised way
 
+export default function two_crystal_balls(breaks: boolean[]): number {
+    const checkIndexJump = Math.floor(Math.sqrt(breaks.length));
+    let i = checkIndexJump;
+
+    for (; i < breaks.length; i += checkIndexJump) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+    i -= checkIndexJump
+
+
+    for (let j = 0; j <= checkIndexJump && i < breaks.length; j++, i++) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+    return -1
 }
