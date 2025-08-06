19ab146810769c300f505076314bc386bb1bf64e
7doublem <mustafamoallim@gmail.com>
2025-08-06 20:15:57 +0100

    compare binary trees

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,18 @@
 export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+    //base case
 
+    // 1. if a and be are both null
+    if (a === null && b === null) {
+        return true
+    }
+    // 2. if a is null or b is null
+    if (a === null || b === null) {
+        return false
+    }
+    // 3. if a does not equal to b
+    if (a.value !== b.value) {
+        return false;
+    }
+
+    return compare(a.left, b.left) && compare(a.right, b.right)
 }
